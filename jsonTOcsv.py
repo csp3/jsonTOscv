3ec3fe61b94fff2ee835de61fe8a57e94e8f1b01
carlos solis p <solis.krlos@gmail.com>
2022-02-05 21:31:14 -0500

    c2

--- jsonTOcsv.py
+++ jsonTOcsv.py
@@ -1,5 +1,24 @@
 import pandas as pd
-print('Espere...')
-df = pd.read_json (r'lista.json')
-df.to_csv (r'lista.csv', index = None)
-print('Listo')
+#import os
+from glob import glob
+
+con = 0
+arc = []  
+r = 0
+print('archivos:\n---------')
+archivos = glob('*.json') 
+for archivo in archivos:
+    arc.append(archivo) 
+    con+=1
+    print( str(con) + ") " + archivo)
+
+while True:
+    print('Ingrese numero: ')
+    r = int(input())
+    if r >= 1 and r<=con:
+        break 
+
+# 
+df = pd.read_json (arc[r-1])
+df.to_csv (r'convertido_Json_Csv.csv', index = None)
+print('\nHecho!!\n')
